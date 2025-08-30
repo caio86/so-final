f64c831fddfd135b9a0e1016dd831bd0d183e7bb
caio86 <caioluiz86@gmail.com>
2025-08-30 01:30:11 -0300

    update script

--- polpot.py
+++ polpot.py
@@ -1,6 +1,11 @@
 #!/usr/bin/env python3
 
+# Altere GTFO para o seu cenário de experimento #
+# Ex: Cenário 1 a 4, altere GTFO para getC1_4 # #
 from pol import GTFO as getCenario
 from pot import create_graph
+
+# # # # # # # # # # # # # # # # # # # # # # # # #
+
 
 create_graph(*getCenario())
